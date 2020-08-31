7497e2b6a22662cff3eac8eb5152e6aacdbe40ac
Yessen Orazbaiuly <yesseneon@gmail.com>
2020-08-31 21:20:50 +0600

    Create layouts and use nested templates for home page

--- main.go
+++ main.go
@@ -2,26 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
-	"text/template"
 )
-
-var tmpl *template.Template
-
-func init() {
-	tmpl = template.Must(template.ParseGlob("templates/*.gohtml"))
-}
 
 func main() {
 	http.HandleFunc("/", home)
-	http.HandleFunc("/shop", shop)
-	http.HandleFunc("/cart", cart)
-	http.HandleFunc("/checkout", checkout)
-	http.HandleFunc("/blog", blog)
-	http.HandleFunc("/contact", contact)
-	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./resources"))))
+	// http.HandleFunc("/shop", shop)
+	// http.HandleFunc("/cart", cart)
+	// http.HandleFunc("/checkout", checkout)
+	// http.HandleFunc("/blog", blog)
+	// http.HandleFunc("/contact", contact)
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./static"))))
 
 	fmt.Println("Server running...")
 	http.ListenAndServe(":8080", nil)
@@ -29,7 +23,9 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		err := tmpl.ExecuteTemplate(w, "index.gohtml", nil)
+		t := findTemplate("home")
+		ctx := "Custom Title"
+		err := t.ExecuteTemplate(w, "index.gohtml", ctx)
 		handleError(w, err)
 	} else if r.Method == http.MethodPost {
 		io.WriteString(w, "POST /")
@@ -38,59 +34,65 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func shop(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		err := tmpl.ExecuteTemplate(w, "shop.gohtml", nil)
-		handleError(w, err)
-	} else if r.Method == http.MethodPost {
-		io.WriteString(w, "POST /shop")
-	} else {
-		http.Error(w, "405 method not allowed", 405)
-	}
-}
+// func shop(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		err := tmpl.ExecuteTemplate(w, "shop.gohtml", nil)
+// 		handleError(w, err)
+// 	} else if r.Method == http.MethodPost {
+// 		io.WriteString(w, "POST /shop")
+// 	} else {
+// 		http.Error(w, "405 method not allowed", 405)
+// 	}
+// }
 
-func cart(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		err := tmpl.ExecuteTemplate(w, "cart.gohtml", nil)
-		handleError(w, err)
-	} else if r.Method == http.MethodPost {
-		io.WriteString(w, "POST /cart")
-	} else {
-		http.Error(w, "405 method not allowed", 405)
-	}
-}
+// func cart(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		err := tmpl.ExecuteTemplate(w, "cart.gohtml", nil)
+// 		handleError(w, err)
+// 	} else if r.Method == http.MethodPost {
+// 		io.WriteString(w, "POST /cart")
+// 	} else {
+// 		http.Error(w, "405 method not allowed", 405)
+// 	}
+// }
 
-func checkout(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		err := tmpl.ExecuteTemplate(w, "checkout.gohtml", nil)
-		handleError(w, err)
-	} else if r.Method == http.MethodPost {
-		io.WriteString(w, "POST /checkout")
-	} else {
-		http.Error(w, "405 method not allowed", 405)
-	}
-}
+// func checkout(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		err := tmpl.ExecuteTemplate(w, "checkout.gohtml", nil)
+// 		handleError(w, err)
+// 	} else if r.Method == http.MethodPost {
+// 		io.WriteString(w, "POST /checkout")
+// 	} else {
+// 		http.Error(w, "405 method not allowed", 405)
+// 	}
+// }
 
-func blog(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		err := tmpl.ExecuteTemplate(w, "blog.gohtml", nil)
-		handleError(w, err)
-	} else if r.Method == http.MethodPost {
-		io.WriteString(w, "POST /blog")
-	} else {
-		http.Error(w, "405 method not allowed", 405)
-	}
-}
+// func blog(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		err := tmpl.ExecuteTemplate(w, "blog.gohtml", nil)
+// 		handleError(w, err)
+// 	} else if r.Method == http.MethodPost {
+// 		io.WriteString(w, "POST /blog")
+// 	} else {
+// 		http.Error(w, "405 method not allowed", 405)
+// 	}
+// }
 
-func contact(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		err := tmpl.ExecuteTemplate(w, "contact.gohtml", nil)
-		handleError(w, err)
-	} else if r.Method == http.MethodPost {
-		io.WriteString(w, "POST /contact")
-	} else {
-		http.Error(w, "405 method not allowed", 405)
-	}
+// func contact(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		err := tmpl.ExecuteTemplate(w, "contact.gohtml", nil)
+// 		handleError(w, err)
+// 	} else if r.Method == http.MethodPost {
+// 		io.WriteString(w, "POST /contact")
+// 	} else {
+// 		http.Error(w, "405 method not allowed", 405)
+// 	}
+// }
+
+func findTemplate(folder string) *template.Template {
+	t := template.Must(template.ParseGlob("templates/layouts/*.gohtml"))
+	t = template.Must(t.ParseGlob("templates/" + folder + "/*.gohtml"))
+	return t
 }
 
 func handleError(w http.ResponseWriter, err error) {
