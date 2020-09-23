f3e7e9cbabd9d44cb3648c9fe8634dc141bca631
Yessen Orazbaiuly <yesseneon@gmail.com>
2020-09-23 13:13:49 +0600

    Format price in the template

--- config/tpl.go
+++ config/tpl.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 )
@@ -8,7 +9,16 @@ import (
 var Tpl *template.Template
 
 func init() {
-	Tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+	funcMap := template.FuncMap{
+		"fmtprice": priceToString,
+	}
+
+	Tpl = template.Must(template.New("").Funcs(funcMap).ParseGlob("templates/*.gohtml"))
 	Tpl = template.Must(Tpl.ParseGlob("templates/admin/*.gohtml"))
 	log.Println("Tpl connected")
+}
+
+func priceToString(price int) string {
+	fprice := (float64(price) + 0.1) / 100
+	return fmt.Sprintf("%.2f", fprice)
 }
