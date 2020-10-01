9fd6a298e4585688e6be1f8c1db7233214bdea79
Yessen Orazbaiuly <yesseneon@gmail.com>
2020-10-01 19:02:40 +0600

    Product filters: hide elements if there are more than 5, and add the 'show more' button

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,69 @@
 'use strict';
 
 (function ($) {
+    showFiveItems("brand");
+    showFiveItems("size");
+    showFiveItems("color");
+
+    function showFiveItems(name) {
+        var len = $("input[name=" + name + "]:checked").length;
+        var counter = 5 - len;
+
+        $.each($("input[name=" + name + "]"), function(k, input) {
+            if (counter <= 0) return false;
+
+            if ($(input).prop("checked") == false) {
+                $(input).closest("label").addClass("show");
+                counter -= 1;
+            }
+        });
+    }
+
+    $(".brand-more").click(function(e) {
+        e.preventDefault();
+        showOrHideItems("brand", $(this));
+    });
+
+    $(".size-more").click(function(e) {
+        e.preventDefault();
+        showOrHideItems("size", $(this));
+    });
+
+    $(".color-more").click(function(e) {
+        e.preventDefault();
+        showOrHideItems("color", $(this));
+    });
+
+    function showOrHideItems(name, self) {
+        if ($(self).data("state") == "close") {
+            $.each($("input[name=" + name + "]"), function(k, input) {
+                if ($(input).prop("checked") == false) {
+                    $(input).closest("label").addClass("show");
+                }
+            });
+
+            $(self).data("state", "open");
+            $(self).html("Hide")
+        } else {
+            var len = $("input[name=" + name + "]:checked").length;
+            var counter = 5 - len;
+
+            $.each($("input[name=" + name + "]"), function(k, input) {
+                if ($(input).prop("checked") == false) {
+                    if (counter > 0) {
+                        counter -= 1;
+                        return true;
+                    }
+
+                    $(input).closest("label").removeClass("show");
+                }
+            });
+
+            $(self).data("state", "close");
+            $(self).html("Show more");
+        }
+    }
+
     // filter for brands
     $("input[name=brand]").click(function() {
         var list = "list";
@@ -10,7 +73,7 @@
             }
         });
 
-        filterProduct(list, /&b=[^(&|\s|#)]+/, "&b=");
+        filterProducts(list, /&b=[^(&|\s|#)]+/, "&b=");
     });
 
     // filter for sizes
@@ -22,7 +85,7 @@
             }
         });
 
-        filterProduct(list, /&s=[^(&|\s|#)]+/, "&s=");
+        filterProducts(list, /&s=[^(&|\s|#)]+/, "&s=");
     });
 
     // filter for colors
@@ -34,10 +97,10 @@
             }
         });
 
-        filterProduct(list, /&c=[^(&|\s|#)]+/, "&c=");
+        filterProducts(list, /&c=[^(&|\s|#)]+/, "&c=");
     });
 
-    function filterProduct(list, regexp, prefix) {
+    function filterProducts(list, regexp, prefix) {
         var url = $(location).attr("href");
 
         if (regexp.test(url)) {
