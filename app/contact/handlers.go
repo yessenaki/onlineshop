a9fe687052e93fa71adc81a03be3fd8e0edc59cc
Yessen Orazbaiuly <yesseneon@gmail.com>
2020-10-19 12:11:19 +0600

    Sending emails with stmp.SendMail

--- app/contact/handlers.go
+++ app/contact/handlers.go
@@ -1,8 +1,9 @@
 package contact
 
 import (
-	"io"
+	"fmt"
 	"net/http"
+	"net/smtp"
 	"onlineshop/helper"
 )
 
@@ -32,7 +33,32 @@ func Index() http.Handler {
 			helper.Render(w, "contact.gohtml", data)
 			return
 		} else if r.Method == http.MethodPost {
-			io.WriteString(w, "POST /")
+			// Sender data.
+			from := "email@gmail.com"
+			password := "password"
+
+			// Receiver email address.
+			to := []string{
+				"email@gmail.com",
+			}
+
+			// smtp server configuration.
+			smtpHost := "smtp.gmail.com"
+			smtpPort := "587"
+
+			// Message.
+			message := []byte("This is a test email message.")
+
+			// Authentication.
+			auth := smtp.PlainAuth("", from, password, smtpHost)
+
+			// Sending email.
+			err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println("Email Sent Successfully!")
 		} else {
 			http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
 			return
